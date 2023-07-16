72e5c923d3b6834ee03312781a0620be6e19e228
Damien Nelson <dhnisaman@gmail.com>
2023-07-16 16:54:19 -0400

    commit tree 055e4e4fd2e6ee9210148c6ba2f366341053d4ad

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,24 @@ class LinkedList
 
   def nth_from_end(n)
     # your code here
+   length = 0
+   node = head
+   until node.nil?
+    node = node.next_node
+    length += 1
+   end
+
+   return nil if n < 1 || n > length
+
+   i = length - n
+
+   node = head
+   
+   i.times do
+    node = node.next_node
+   end
+
+   node.value
   end
 
 end
